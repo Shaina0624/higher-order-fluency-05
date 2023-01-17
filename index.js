c71de7b1d40c9a6377f299045bbc0ef03a3f9f06
Shaina0624 <shainaguzman0624@hmail.com>
2023-01-17 15:41:12 +0000

    done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const averageAge = arr => {
+    return Math.round(arr.reduce((x, y) => x + y.age,0) / arr.length);
+};
 
+const orderedAlumni = arr => {
+    return arr.sort((a, b) => b.age - a.age);
+};
+
+const allUseJavaScript = arr => {
+    return arr.every(x => x.language === 'JavaScript');
+};
